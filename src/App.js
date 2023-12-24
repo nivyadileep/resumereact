1e9aa37236fd185005a37f063de01cf9f65666ad
nivyadileep <75192803+nivyadileep@users.noreply.github.com>
2023-12-24 16:52:57 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Resume from './components/resume';
+
+
+function App() {
+  
+  return (
+    <div>
+      <Resume />
+    </div>
+  );
+}
+
+export default App;
+
+
