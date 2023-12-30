856e25a63b047f5cc392b8dd92ecd6848d0de5f9
nivyadileep <75192803+nivyadileep@users.noreply.github.com>
2023-12-30 15:44:02 +0530

    Create data.js

---
+++ src/components/resume/data.js
@@ -0,0 +1,71 @@
+const resumeData = {
+    name: "Nivya Dileep",
+    shortLogoName: "ND",
+    location: "Kerala, India",
+    phone: "+91-9207101474",
+    mail: "nivyadileep2000.eee@gmail.com",
+    Linkedin: "linkedin.com/in/nivyadileep",
+    objective:
+      "An engineering graduate from Mar Athanasius College of Engineering, seeking a position to contribute my technical and innovative problem-solving skills. Hands on experience in HTML, CSS, Javascript & React. Eager to contribute to dynamic engineering projects.",
+    skills: [
+      "HTML",
+      "CSS",
+      "Javascript",
+      "React",
+      "Data analytics",
+      "SQL",
+      "Python",
+      "Tableau",
+      "Project Management",
+      "Team Management",
+      "Communication",
+    ],
+    education: {
+      college: "Mar Athanasisus College of Engineering",
+      stream: "Bachelor of Technology | Electrical & Electronics",
+      cgpa: 8.08,
+      internships: [
+        "Computer Architecture, Keltron",
+        "Machine Learning, Verzeo",
+        "Data Analytics Traineeship, MedTourEasy",
+      ],
+      awards: [
+        "Recipient of IEEE PES Global Humanitarian Project , 6th prize",
+        "Rashtrapathi award - Bharat Scout and Guides",
+      ],
+      personalProjects: [
+        { name: "Resume builder", link: "https://github.com/nivyadileep/resumereact" },
+      ],
+    },
+    projects: [
+      {
+        name: "PROJECT 1",
+        title: "Robust Control of Variable Speed BLDC motor using PID controller",
+        description:
+          "This project aimed to design, implement, and evaluate a control system for a Brushless DC (BLDC) motor drive capable of operating at variable speeds. The main focus was on achieving efficient control using a Proportional-Integral-Derivative (PID) controller.",
+      },
+      {
+        name: "PROJECT 2",
+        title: "Smart Assistant for Visually Impaired",
+        description:
+          "This project aimed to create an intelligent assistant to support individuals with visual impairments in their daily activities, utilizing machine learning techniques.",
+        notes: ["Face Recognition", "Object detection", "Text to speech"],
+      },
+      {
+        name: "PROJECT 3",
+        title: "Bluetooth-Controlled Robotic Car using Arduino",
+      },
+    ],
+    workHistory: [
+      {
+        role: "GROWTH ASSOCIATE",
+        duration: "10/2022 to CURRENT",
+        company: "Entri App | Kochi,Kerala",
+        summary:
+          "As a growth associate, my responsibility typically revolve around driving and optimizing the growth of a company. Utilized data analytics tools to collect and analyze data related to user behavior, market trends, and performance metrics. Identify and implement strategies to acquire new customers or users.",
+        skills: ["Team Management", "Marketing Analytics", "Growth Hacking"],
+      },
+    ],
+  };
+
+export default resumeData
