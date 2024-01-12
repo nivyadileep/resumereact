210636f7541351109153847d7efbf6174f354810
nivyadileep <75192803+nivyadileep@users.noreply.github.com>
2024-01-12 16:41:32 +0530

    Update data.js

--- src/components/resume/data.js
+++ src/components/resume/data.js
@@ -1,71 +1,79 @@
 const resumeData = {
-    name: "Nivya Dileep",
-    shortLogoName: "ND",
-    location: "Kerala, India",
-    phone: "+91-9207101474",
-    mail: "nivyadileep2000.eee@gmail.com",
-    Linkedin: "linkedin.com/in/nivyadileep",
-    objective:
-      "An engineering graduate from Mar Athanasius College of Engineering, seeking a position to contribute my technical and innovative problem-solving skills. Hands on experience in HTML, CSS, Javascript & React. Eager to contribute to dynamic engineering projects.",
-    skills: [
-      "HTML",
-      "CSS",
-      "Javascript",
-      "React",
-      "Data analytics",
-      "SQL",
-      "Python",
-      "Tableau",
-      "Project Management",
-      "Team Management",
-      "Communication",
+  name: "Nivya Dileep",
+  shortLogoName: "ND",
+  location: "Kerala, India",
+  phone: "+91-9207101474",
+  mail: "nivyadileep2000.eee@gmail.com",
+  Linkedin: "https://www.linkedin.com/in/nivyadileep",
+  objective: 
+    "An engineering graduate from Mar Athanasius College of Engineering, seeking a position to contribute my technical and innovative problem-solving skills. Hands-on experience in circuit design, control systems, and software programming.",
+  skills: [
+    "BLDC Motor Drive Circuit",
+    "Arduino",
+    "Raspberry Pi",
+    "Matlab",
+    "Data analytics",
+    "SQL",
+    "Python",
+    "Tableau",
+    "Project Management",
+    "Team Management",
+    "Communication",
+  ],
+  education: {
+    college: "Mar Athanasisus College of Engineering",
+    stream: "Bachelor of Technology | Electrical & Electronics",
+    cgpa: 8.08,
+    internships: [
+      {name: "Computer Architecture, Keltron", link: ""},
+      {name: "Machine Learning, Verzeo", link: ""},
+      {name: "Data Analytics Traineeship, MedTourEasy", link: "https://github.com/nivyadileep/data-analytics"},
     ],
-    education: {
-      college: "Mar Athanasisus College of Engineering",
-      stream: "Bachelor of Technology | Electrical & Electronics",
-      cgpa: 8.08,
-      internships: [
-        "Computer Architecture, Keltron",
-        "Machine Learning, Verzeo",
-        "Data Analytics Traineeship, MedTourEasy",
-      ],
-      awards: [
-        "Recipient of IEEE PES Global Humanitarian Project , 6th prize",
-        "Rashtrapathi award - Bharat Scout and Guides",
-      ],
-      personalProjects: [
-        { name: "Resume builder", link: "https://github.com/nivyadileep/resumereact" },
-      ],
+    awards: [
+      "Recipient of IEEE PES Global Humanitarian Project , 6th prize",
+      "IEDC E-Novus Project Expo Winners",
+      "Runner-up in project idea presentation by CET",
+      "Rashtrapathi award - Bharat Scout and Guides",
+    ],
+    responsibilities: [
+      "IEEE Documentation Commitee Member",
+      "Event Coordinator: Coordinated roughly 50 technical events during the National Level Technical Fest - Takshak",
+      "Event Head: Was able to bring the speaker and 300+ registrations",
+    ],
+    
+
+    personalProjects: [
+      {name: "Resume Builder", link: "https://github.com/nivyadileep/resumereact"},
+      {name: "Periods Tracker", link: "https://github.com/nivyadileep/periods-tracker"},
+    ],
+  },
+  projects: [
+    {
+      name: "PROJECT 1",
+      title: "Robust Control of Variable Speed BLDC motor using PID controller",
+      link:"",
+      description:
+        "This project aimed to design, implement, and evaluate a control system for a Brushless DC (BLDC) motor drive capable of operating at variable speeds. The main focus was on achieving efficient control using a Proportional-Integral-Derivative (PID) controller.",
     },
-    projects: [
-      {
-        name: "PROJECT 1",
-        title: "Robust Control of Variable Speed BLDC motor using PID controller",
-        description:
-          "This project aimed to design, implement, and evaluate a control system for a Brushless DC (BLDC) motor drive capable of operating at variable speeds. The main focus was on achieving efficient control using a Proportional-Integral-Derivative (PID) controller.",
-      },
-      {
-        name: "PROJECT 2",
-        title: "Smart Assistant for Visually Impaired",
-        description:
-          "This project aimed to create an intelligent assistant to support individuals with visual impairments in their daily activities, utilizing machine learning techniques.",
-        notes: ["Face Recognition", "Object detection", "Text to speech"],
-      },
-      {
-        name: "PROJECT 3",
-        title: "Bluetooth-Controlled Robotic Car using Arduino",
-      },
-    ],
-    workHistory: [
-      {
-        role: "GROWTH ASSOCIATE",
-        duration: "10/2022 to CURRENT",
-        company: "Entri App | Kochi,Kerala",
-        summary:
-          "As a growth associate, my responsibility typically revolve around driving and optimizing the growth of a company. Utilized data analytics tools to collect and analyze data related to user behavior, market trends, and performance metrics. Identify and implement strategies to acquire new customers or users.",
-        skills: ["Team Management", "Marketing Analytics", "Growth Hacking"],
-      },
-    ],
-  };
+    {
+      name: "PROJECT 2",
+      title: "Smart Assistant for Visually Impaired",
+      link: "https://github.com/nivyadileep/Btech-Projects",
+      description:
+        " An intelligent assistant to support individuals with visual impairments in their daily activities, utilizing machine learning techniques.",
+      notes: ["Face Recognition", "Object detection", "Text to speech"],
+    },
+    
+  ],
+  workHistory: [
+    {
+      role: "GROWTH ASSOCIATE",
+      duration: "10/2022 to CURRENT",
+      company: "Entri App | Kochi,Kerala",
+      summary:
+        "Utilized data analytics tools to collect and analyze data related to user behavior, market trends, and performance metrics. Identify and implement strategies to acquire new customers or users.", 
+    },
+  ],
+};
 
 export default resumeData
